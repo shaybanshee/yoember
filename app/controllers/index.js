59cfb92eaceb62dc3345688717fea31662fc4df5
Shani Banerjee <shanibanerjee@gmail.com>
2019-11-26 16:08:37 -0500

    Added isValid logic to index controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,12 @@
-import { computed, observer } from '@ember/object';
 import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
 
-  isDisabled: true,
 
   emailAddress: '',
 
-  actualEmailAddress: computed('emailAddress', function(){
-    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
-  }),
-
-  emailAddressChanged: observer('emailAddress', function() { 
-    console.log('observer is called', this.get('emailAddress')); 
-  })
-
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid')
+  
 });
