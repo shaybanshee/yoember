7c41f1452049e5f572c9d2ab8c3cab06e476b7e2
Shani Banerjee <shanibanerjee@gmail.com>
2019-11-26 16:57:39 -0500

    Added save invitation method

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,10 +3,19 @@ import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
 
-
+  responseMessage: '',
   emailAddress: '',
 
   isValid: match('emailAddress', /^.+@.+\..+$/),
-  isDisabled: not('isValid')
-  
+  isDisabled: not('isValid'),
+
+  actions: {
+
+   saveInvitation() {
+     alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+     this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+     this.set('emailAddress', '');
+   }
+ }
+
 });
