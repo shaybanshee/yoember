ae7f025ec910dbc7da6f9ed3eb2d752f9c6f0e5d
Shani Banerjee <shanibanerjee@gmail.com>
2019-12-03 17:04:27 -0500

    Initial contact page built

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,4 +1,20 @@
 import Controller from '@ember/controller';
+import { match, not } from '@ember/object/computed';
 
 export default Controller.extend({
+
+  contactHeader: 'Contact',
+  emailAddress : '',
+  text: '',
+
+  isValid: match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: not('isValid'),
+
+  actions: {
+  sendMessage() {
+    alert(`Sending of your message is in progress: ${this.get('emailAddress')}`);
+    this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
+    this.set('emailAddress', '');
+    }
+  }
 });
