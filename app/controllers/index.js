e2a4c618e2b26c85768aa722a1f4d14288cdab16
Shani Banerjee <shanibanerjee@gmail.com>
2019-12-05 14:00:01 -0500

    Added firebase key to enviro file

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -13,10 +13,17 @@ export default Controller.extend({
   actions: {
 
    saveInvitation() {
-     alert(`Saving of the following email address is in progress: ${this.get('emailAddress')}`);
+     const email = this.get('emailAddress')
+
+     const newInvitation = this.store.createRecord('invitation', { email });
+
+     newInvitation.save().then(response => {
+
      this.set('responseMessage', `Thank you! We've just saved your email address: ${this.get('emailAddress')}`);
      this.set('emailAddress', '');
-   }
+    });
+
+  }
  }
 
 });
--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,17 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: "AIzaSyDDyIHyphU2XGPOEfZ6DijSce4-Lfr81-I",
+      authDomain: "library-app-1b391.firebaseapp.com",
+      databaseURL: "https://library-app-1b391.firebaseio.com",
+      projectId: "library-app-1b391",
+      storageBucket: "library-app-1b391.appspot.com",
+      messagingSenderId: "19480544744",
+      appId: "1:19480544744:web:91d7b27700829241e6d05d"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
